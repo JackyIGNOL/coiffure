36427969a21a2ed0a067a1f6f9a61b0ac1db6a20
Luidjy NERTOMB <nertomb24@outlook.com>
2020-12-02 12:24:00 +0100

    modification de la nav bar et des trois premiers menuburgers

--- src/Controller/CoiffureController.php
+++ src/Controller/CoiffureController.php
@@ -24,4 +24,77 @@ class CoiffureController extends AbstractController
     {
         return $this->render("coiffure/home.html.twig");
     }
+
+    /**
+     * @Route("/coiffure/autre/creation", name="creation")
+     */
+
+    public function creation()
+    {
+        return $this->render('coiffure/autre/creation.html.twig');
+    }
+
+    /**
+     * @Route("/coiffure/autre/quiSommeNous", name="quiSommeNous")
+     */
+
+    public function quiSommeNous()
+    {
+        return $this->render('coiffure/autre/quiSommeNous.html.twig');
+    }
+
+    /**
+     * @Route("/coiffure/autre/actualites", name="actualites")
+     */
+
+    public function actualites()
+    {
+        return $this->render('coiffure/autre/actualites.html.twig');
+    }
+
+    /**
+     * @Route("/coiffure/Books/coupeEnfant", name="coupeEnfant")
+     */
+
+    public function coupeEnfant()
+    {
+        return $this->render('coiffure/Books/coupeEnfant.html.twig');
+    }
+
+     /**
+     * @Route("/coiffure/Books/coupeFemme", name="coupeFemme")
+     */
+
+    public function coupeFemme()
+    {
+        return $this->render('coiffure/Books/coupeFemme.html.twig');
+    }
+
+    /**
+     * @Route("/coiffure/Books/coupeHomme", name="coupeHomme")
+     */
+
+    public function coupeHomme()
+    {
+        return $this->render('coiffure/Books/coupeHomme.html.twig');
+    }
+
+    /**
+     * @Route("/coiffure/Tarifs/tarifsStandars", name="tarifsStandars")
+     */
+
+    public function tarifsStandars()
+    {
+        return $this->render('coiffure/Tarifs/tarifsStandars.html.twig');
+    }
+
+    /**
+     * @Route("/coiffure/Tarifs/Produits", name="Produits")
+     */
+
+    public function Produits()
+    {
+        return $this->render('coiffure/Tarifs/Produits.html.twig');
+    }
+
 }
