66f2621183df4e4aa711d946a9623707eca633e9
Jacky IGNOL <jacky.ignol@gmail.com>
2020-12-04 13:31:25 +0100

    debut rendez vous featrure

---
+++ src/Entity/Rendezvous.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RendezvousRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RendezvousRepository::class)
+ */
+class Rendezvous
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $heureAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $occupé;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=user::class, inversedBy="rendezvouses")
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getHeureAt(): ?\DateTimeInterface
+    {
+        return $this->heureAt;
+    }
+
+    public function setHeureAt(\DateTimeInterface $heureAt): self
+    {
+        $this->heureAt = $heureAt;
+
+        return $this;
+    }
+
+    public function getOccupé(): ?bool
+    {
+        return $this->occupé;
+    }
+
+    public function setOccupé(bool $occupé): self
+    {
+        $this->occupé = $occupé;
+
+        return $this;
+    }
+
+    public function getUser(): ?user
+    {
+        return $this->user;
+    }
+
+    public function setUser(?user $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
