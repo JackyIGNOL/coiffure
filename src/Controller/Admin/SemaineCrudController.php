679eef642a75aa6736af08f662e8cc7608151787
Jacky IGNOL <jacky.ignol@gmail.com>
2020-12-14 16:21:45 +0100

    admin encore

---
+++ src/Controller/Admin/SemaineCrudController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Entity\Semaine;
+use App\Entity\Rendezvous;
+use DateTime;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class SemaineCrudController extends AbstractCrudController
+{
+
+
+    public static function getEntityFqcn(): string
+    {
+        return Semaine::class;
+    }
+
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
