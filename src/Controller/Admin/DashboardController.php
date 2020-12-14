30299e0cdf31dc09d6672233a8313288790f6845
Jacky IGNOL <jacky.ignol@gmail.com>
2020-12-14 09:34:53 +0100

    petit test css et admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use DateTime;
+use App\Entity\User;
+use App\Entity\Rendezvous;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -16,7 +19,33 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        //dd($this->getUser());
+        $repositoryuser = $this->getDoctrine()->getRepository(User::class);
+        $repositoryrdv = $this->getDoctrine()->getRepository(Rendezvous::class);
+        $lesusers = $repositoryuser->findAll();
+        $lesrendezvous = $repositoryrdv->findAll();
+
+
+        $leshoraires = [];
+        for ($i = 8; $i <= 11; $i++) {
+            array_push($leshoraires, $i);
+        }
+        $now   = new DateTime;
+        for ($i = 1; $i < 8; $i++) {
+            $clone = clone $now;
+            $modif = '+' . $i . ' day';
+            $clone->modify($modif);
+            foreach ($leshoraires as $heure) {
+                $clone2 = clone $clone;
+                $clone2->setTime($heure, 0, 0);
+                $lesjh[$i][$heure] = $clone2;
+            }
+        }
+
+
+
+        //dd($lesusers,$lesrendezvous);
+
+
         // redirect to some CRUD controller
         /* $routeBuilder = $this->get(CrudUrlGenerator::class)->build(); */
 
@@ -29,7 +58,11 @@ class DashboardController extends AbstractDashboardController
 
         // you can also render some template to display a proper Dashboard
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        return $this->render('admin\dashboard.html.twig');
+        return $this->render('admin\dashboard.html.twig', [
+            'lesrendezvous' => $lesrendezvous,
+            'lesusers' => $lesusers,
+            'lesjh' => $lesjh
+        ]);
         /* return parent::index(); */
     }
 
