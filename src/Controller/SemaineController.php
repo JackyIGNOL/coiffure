aa619e92301b4fb6f344a691ec2c68c4cb921753
Jacky IGNOL <jacky.ignol@gmail.com>
2020-12-16 09:18:51 +0100

    Admin semaine ajout des heures dispo

--- src/Controller/SemaineController.php
+++ src/Controller/SemaineController.php
@@ -30,15 +30,39 @@ class SemaineController extends AbstractController
     /**
      * @Route("/semaine/ajout/{jour}/{heure}", name="ajoutheure")
      */
-    public function ajoutheure(String $jour, int $heure, ObjectManager $objectManager)
+    public function ajoutheure($jour, $heure, ObjectManager $objectManager)
     {
         $repositorysemaine = $this->getDoctrine()->getRepository(Semaine::class);
+        /* dd($repositorysemaine); */
         $lejour = $repositorysemaine->find($jour);
+        $lejour2 = $repositorysemaine->findAll();
+        /* $lejour3 = $repositorysemaine->findBy(array($jour)); */
+
+
+
+        foreach ($lejour2 as $jour2) {
+            if ($jour2->getJour() == $jour) {
+                if ($jour2->getHoraire() == null) {
+                    $leshoraires = [];
+                    /* dd($jour2, $leshoraires); */
+                } else {
+                    $leshoraires = $jour2->getHoraire();
+                }
+                array_push($leshoraires, $heure);
+                $jour2->setHoraire($leshoraires);
+                /* dd($lejour2, $jour, $jour2, $leshoraires); */
+                /* array_push($horaire, $heure); */
+                $objectManager->persist($jour2);
+                $objectManager->flush();
+            }
+        }
+        /* 
+        dd($lejour, $lejour2, $jour, $heure);
         $horaire = $lejour->getHoraire();
         array_push($horaire, $heure);
-
+        $lejour->setHoraire($horaire);
         $objectManager->persist($lejour);
-        $objectManager->flush();
+        $objectManager->flush(); */
         return $this->redirectToRoute("semaine");
     }
 }
